cdf8c7f348abf3ff947c07cc4bf500ea7c5f0c0c
Gabriel <gabriel.beck.santos@gmail.com>
2013-03-21 16:48:30 -0300

    Upgrade Recent Activity List

--- n3phele-mobile-android/assets/www/RecentActivityList.js
+++ n3phele-mobile-android/assets/www/RecentActivityList.js
@@ -87,13 +87,16 @@ enyo.kind({
 					{tag: "span", content: "Command: ", style:"font-variant:small-caps;"}, 
 					{name: "acComDesc", style:"display: inline-block"},
 					{tag: "br"},
-					{tag: "span", content: "Executed From ", style:"font-variant:small-caps;"}, 
+					{tag: "span", content: "Started: ", style:"font-variant:small-caps;"}, 
 					{name: "acStart", style:"display: inline-block"},
-					{tag: "span", content: " to ", style:"font-variant:small-caps;"}, 
+					{tag : "br"},
+					{tag: "span", content: "Completed: ", style:"font-variant:small-caps;"}, 
 					{name: "acComplete", style:"display: inline-block"},
 					{tag: "br"},
-					{tag: "span", content: "Duration[in milliseconds]: ", style:"font-variant:small-caps;"}, //seconds
+					{tag: "span", content: "Duration(in minutes): ", style:"font-variant:small-caps;"}, //seconds
 					{name: "acDuration", style:"display: inline-block"},
+					{tag: "br"},
+					{name: "divider", classes: "list-divider"},
 					{tag: "br"},
 					{tag: "span", content: "Log: ", style:"font-variant:small-caps;"},
 
@@ -114,28 +117,34 @@ enyo.kind({
 			ajaxComponent
 			.go({'summary' : true, 'start' : 0, 'end' : 10})
 			.response( this, function(sender, response){
-				
+						
 				this.$.acName.setContent(" "+response.name);
 				this.$.acStatus.setContent(" "+response.state );
 				this.$.acComDesc.setContent(" "+response.description);
-				this.$.acStart.setContent(" "+response.start);
-				this.$.acComplete.setContent(" "+response.complete);
 				
 				var d1 = new Date(response.start);
 				var d2 = new Date(response.complete);
 				
-				this.$.acDuration.setContent(" "+(d2-d1));
+				this.$.acStart.setContent(" "+d1.getFullYear()+"-"+(d1.getMonth()+1)+"-"+d1.getDate()+" "+d1.getHours()+":"+d1.getMinutes());
+				this.$.acComplete.setContent(" "+d2.getFullYear()+"-"+(d2.getMonth()+1)+"-"+d2.getDate()+" "+d2.getHours()+":"+d2.getMinutes());
+				
+				this.$.acDuration.setContent(" "+((d2-d1)/60000));
 				
 				var narrative = fixArrayInformation(response.narrative);
 				
 				for( var i in narrative ){
 					console.log("oi nar",narrative[i] );
 					this.$.panel_three.createComponent({tag: "br"});
-					this.$.panel_three.createComponent({style:"display: inline-block;", content: "  [ "+narrative[i].stamp+" ]  "});
-					this.$.panel_three.createComponent({style:"display: inline-block;font-weight: bold;", content : "  ( "+narrative[i].state+" ) "});
-					this.$.panel_three.createComponent({style:"display: inline-block;", content : " "+narrative[i].tag+" : "});
-					this.$.panel_three.createComponent({style:"display: inline-block;font-weight: bold", content : " "+narrative[i].text});
+					var stamp = new Date(narrative[i].stamp);
+					if(narrative[i].state=="info"){
+					this.$.panel_three.createComponent({kind:"Image", src:"assets/info.png", fit: true, style:"display: inline-block;"});
 				}
+					this.$.panel_three.createComponent({style:"display: inline-block;", content: "  [ "+stamp.getFullYear()+"-"+(stamp.getMonth()+1)+"-"+stamp.getDate()+" "+stamp.getHours()+":"+stamp.getMinutes()+" ]  "});
+					this.$.panel_three.createComponent({style:"display: inline-block;", content : " "+narrative[i].tag+" : "});
+					this.$.panel_three.createComponent({style:"display: inline-block;font-weight: bold;", content : " "+narrative[i].text});
+					this.$.panel_three.createComponent({tag: "br"});
+				}
+				
 				this.$.panel_three.render();
 				this.reflow();
 				
