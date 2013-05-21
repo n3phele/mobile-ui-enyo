6bb970515acefb2d8ad59771ccf7b5c3419bcb96
RodrigoKGC <rodrigokgc@gmail.com>
2013-05-21 14:31:53 -0300

    Chart working

--- n3phele-mobile-android/assets/www/Chart.js
+++ n3phele-mobile-android/assets/www/Chart.js
@@ -28,10 +28,23 @@ enyo.kind({
     ],
     create:function() {
         this.inherited(arguments);
-      
         // a null signifies separate line segments
-        var d1 = [[0, 4], [2, 2], [4,6] ,[4, 5], [6, 4], [7,10]]; //var d1 = [[0, 12], [24, 10], [4,2] , [7, 2.5], [12, 14], [24, 50]];
+        //var d1 = [[0, 4], [2, 2], [4,6] ,[4, 5], [6, 4], [7,10]]; //var d1 = [[0, 12], [24, 10], [4,2] , [7, 2.5], [12, 14], [24, 50]];
+		var chartData = [];
         
-        this.$.flot.setData([d1]);
+		for(var i = 0; i < this.data.length; i++){
+			chartData[i] = [i, parseFloat(this.data[i])];
+		}
+
+        if (this.type == "Cumulative Cost") {
+            for (var i = 1; i < chartData.length; i++) {
+                chartData[i][1] = chartData[i][1] + chartData[i - 1][1];
+            }
+        }
+		
+        console.log(chartData);
+
+        this.$.flot.setData([chartData]);
+		
     }
 });
